6a001088a030918cf2ce0022385de1abd61aa842
ivan-cukic <ivan-cukic@533d9b8e-d317-0410-bd64-8f4d54abdf9f>
2008-10-24 09:30:41 +0000

    Working on the avatar button
    git-svn-id: http://jabbin.svn.sourceforge.net/svnroot/jabbin/joimtrunk@328 533d9b8e-d317-0410-bd64-8f4d54abdf9f

--- src/tools/customwidgets/selfavatar.cpp
+++ src/tools/customwidgets/selfavatar.cpp
@@ -18,17 +18,112 @@
  */
 
 #include "selfavatar.h"
+#include <QToolButton>
+#include <QMenu>
+#include <QDir>
+#include <QDebug>
+#include <QList>
+
+#define MENU_AVATAR_ICON_SIZE QSize(32, 32)
+#define MENU_AVATAR_COLUMN_COUNT 5
+#define MENU_AVATAR_ROW_COUNT_MAX 7
+
+class SelfAvatarMenu: public QMenu {
+public:
+    SelfAvatarMenu(SelfAvatar * parent)
+        : QMenu(parent), q(parent)
+    {
+        grid = new QGridLayout(this);
+        grid->setMargin(style()->pixelMetric(QStyle::PM_MenuPanelWidth, 0, this));
+        grid->setSpacing(0);
+
+        custom = new QToolButton(this);
+        custom->setText(tr("Choose a custom avatar..."));
+        custom->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
+
+        buttonStyle = CustomWidgets::Common::readFile(":/customwidgets/data/selfavatarmenu_button_style.css");
+    }
+
+    ~SelfAvatarMenu()
+    {
+    }
+
+    void updateMenu()
+    {
+        QList < QDir > dirs;
+        dirs << QDir(":images/avatars");
+
+        int count = 0;
+
+        grid->removeWidget(custom);
+
+        foreach (QDir dir, dirs) {
+            foreach(QString avatarName, dir.entryList()) {
+                if (count >= MENU_AVATAR_COLUMN_COUNT * MENU_AVATAR_ROW_COUNT_MAX) break;
+
+                QIcon avatar = QIcon(dir.absoluteFilePath(avatarName));
+
+                if (buttons.size() > count) {
+                    buttons.at(count)->setIcon(avatar);
+                } else {
+                    QToolButton * button = new QToolButton(this);
+                    button->setIconSize(MENU_AVATAR_ICON_SIZE);
+                    button->setIcon(avatar);
+                    button->setStyleSheet(buttonStyle);
+                    button->installEventFilter(this);
+                    buttons.append(button);
+                    grid->addWidget(button, count / MENU_AVATAR_COLUMN_COUNT, count % MENU_AVATAR_COLUMN_COUNT);
+                    connect(button, SIGNAL(clicked()),
+                            this, SLOT(close()));
+                }
+
+                ++count;
+            }
+        }
+
+        grid->addWidget(custom, count / MENU_AVATAR_COLUMN_COUNT + 1, 0, 1, MENU_AVATAR_COLUMN_COUNT);
+
+        while (buttons.size() > count) {
+            QToolButton * button = buttons.takeLast();
+            delete button;
+        }
+    }
+
+    bool eventFilter(QObject * object, QEvent * event)
+    {
+        if (event->type() == QEvent::MouseButtonRelease) {
+            QToolButton * button = static_cast < QToolButton * > (object);
+            if (buttons.contains(button)) {
+                q->setIcon(button->icon());
+            }
+        }
+        return QMenu::eventFilter(object, event);
+    }
+
+
+private:
+    QList < QToolButton * > buttons;
+    QToolButton * custom;
+    SelfAvatar * q;
+    QGridLayout * grid;
+    QString buttonStyle;
+};
 
 class SelfAvatar::Private {
 public:
     Private()
+        : menu(NULL)
     {
     }
+
+    SelfAvatarMenu * menu;
 };
 
 SelfAvatar::SelfAvatar(QWidget * parent)
     : CustomWidgets::AvatarButton(parent), d(new Private())
 {
+    connect(this, SIGNAL(clicked()),
+            this, SLOT(popupAvatarsMenu()));
 }
 
 SelfAvatar::~SelfAvatar()
@@ -48,6 +143,12 @@ void SelfAvatar::setSelfContact(PsiContact * contact)
 
 void SelfAvatar::popupAvatarsMenu()
 {
-    // TODO
+    if (!d->menu) {
+        d->menu = new SelfAvatarMenu(this);
+    }
+
+    d->menu->updateMenu();
+    d->menu->exec(mapToGlobal(rect().bottomLeft()));
 }
+
 
